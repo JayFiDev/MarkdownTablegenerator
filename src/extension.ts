c7c094b57391dc4649d29505ba099fd1d529bf97
Juergen Fink <jnfink@googlemail.com>
2018-06-11 19:08:18 +0200

    Insert all necessary functions, refactoring could be useful
    -> missing error-handling

--- src/extension.ts
+++ src/extension.ts
@@ -14,11 +14,68 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let disposable = vscode.commands.registerCommand('extension.generateTable', () => {
 
+        function editText(text : string ) {
+			let editor = vscode.window.activeTextEditor;
+			let insertPosition : vscode.Position = editor.selection.active;
+
+			//edit text
+			editor.edit(edit => {
+				edit.insert(insertPosition, text);
+			});		
+        }
+        
+        function generateString(rows, columns) : string {
+            let base_header =       "       |";
+            let base_seperator =    "  ---  |";
+            
+            var string_header = "|";
+            var string_seperator = "|";
+            var string_base = "";
+
+            
+            for (var _i = 0; _i < columns; _i++) {
+                string_header = string_header + base_header;
+                string_seperator = string_seperator + base_seperator;
+            }
+
+            for(var _j = 0; _j < rows; _j++){
+                string_base = string_base + string_header + '\n';
+            }
+            console.log(string_header);
+            console.log(string_seperator);
+            console.log(string_base);
+
+            return string_header + '\n' + string_seperator + '\n' + string_base;
+        }
+
+        
+        // The code you place here will be executed every time your command is executed
+        var user_input;
+        var columns = 3;     //default init to 3
+        var rows = 3;        //default init to 3
+
+
+        let options: vscode.InputBoxOptions = {
+            prompt: "Please insert size of table: \"Rows,Columns\" ",
+            placeHolder: "3,3"
+        }
+        
+        vscode.window.showInputBox(options).then(value => {
+            console.log(value);
+            user_input = value.split(',');
+            rows = user_input[0];
+            columns = user_input[1];
+            console.log(user_input);
+
+            
+            editText(generateString(rows, columns));
+            vscode.window.showInformationMessage('New markdown table was created...');
+        });
+        
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        
     });
 
     context.subscriptions.push(disposable);
@@ -27,3 +84,5 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
+
+
