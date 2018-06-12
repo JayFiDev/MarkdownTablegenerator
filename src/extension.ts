9deb7764d0ab9f23ad2a17785f3bda66abc4eb31
Juergen Fink <jnfink@googlemail.com>
2018-06-12 17:40:31 +0200

    Refactored, started implementation of alignment functions

--- src/extension.ts
+++ src/extension.ts
@@ -3,82 +3,98 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function editText(text : string ) : boolean {
+    let editor = vscode.window.activeTextEditor;
+    
+    if(editor !== undefined){
+        let insertPosition : vscode.Position = editor.selection.active;
+        editor.edit(edit => {
+            edit.insert(insertPosition, text);
+        });	
+        return true;
+    } else {
+        vscode.window.showInformationMessage('Please open a file before generating the table');
+        return false;
+    }
+}
+
+function generateString(rows, columns) : string {
+
+    let base_header =       "       |";
+    let base_seperator =    "  ---  |";
+
+    var string_header = "|" + base_header.repeat(columns);
+    var string_seperator = "|" + base_seperator.repeat(columns);
+    var string_base  = (string_header + '\n').repeat(rows);
+
+    return string_header + '\n' + string_seperator + '\n' + string_base;
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "tablegenerator" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.generateTable', () => {
-
-        function editText(text : string ) : boolean {
-			let editor = vscode.window.activeTextEditor;
-			
-			if(editor !== undefined){
-                let insertPosition : vscode.Position = editor.selection.active;
-                editor.edit(edit => {
-                    edit.insert(insertPosition, text);
-                });	
-                return true;
-            } else {
-                vscode.window.showInformationMessage('Please open a file before generating the table');
-                return false;
-            }
-        }
-        
-        function generateString(rows, columns) : string {
-
-            let base_header =       "       |";
-            let base_seperator =    "  ---  |";
-
-            var string_header = "|" + base_header.repeat(columns);
-            var string_seperator = "|" + base_seperator.repeat(columns);
-            var string_base  = (string_header + '\n').repeat(rows);
-
-            return string_header + '\n' + string_seperator + '\n' + string_base;
-        }
-        
-        // The code you place here will be executed every time your command is executed
-        var user_input;
-        var columns = 3;     //default init to 3
-        var rows = 3;        //default init to 3
-
-
-        let options: vscode.InputBoxOptions = {
-            prompt: "Please insert size of table: \"Rows,Columns\" ",
-            placeHolder: "3,3"
-        };
-        
-        vscode.window.showInputBox(options).then(value => {
-            console.log(value);
-            var regexp = new RegExp('[0-9]+(,[0-9]+)');
-            if(regexp.test(value)){
-                user_input = value.split(',');
-                rows = user_input[0];
-                columns = user_input[1];
-                console.log(user_input);
-
-                
-                if(editText(generateString(rows, columns))) {
-                    vscode.window.showInformationMessage('New markdown table was created...');
+        let disposable1 = vscode.commands.registerCommand('extension.generateTableWithAlignment', () => {
+            let options: vscode.InputBoxOptions = {
+                prompt: "Please insert size of table: \"Rows,Columns\" ",
+                placeHolder: "3,3"
+            };
+            
+            var return_value;
+            vscode.window.showInputBox(options).then(value => {
+                var  { valid, rows, columns } = ParseInput(value);
+                if(valid){
+                    if (editText(generateString(rows, columns))) {
+                        vscode.window.showInformationMessage('New markdown table was created...');
+                    }
                 }
-            } else {
-                vscode.window.showInformationMessage('Wrong input format, please use \"Rows,Columns\"');
-            }
-            
-            
+                  
+            });
         });
+
+
+        let disposable = vscode.commands.registerCommand('extension.generateTable', () => {
         
-        // Display a message box to the user
-        
-    });
+            let options: vscode.InputBoxOptions = {
+                prompt: "Please insert size of table: \"Rows,Columns\" ",
+                placeHolder: "3,3"
+            };
+            
+            var return_value;
+            vscode.window.showInputBox(options).then(value => {
+                var  { valid, rows, columns } = ParseInput(value);
+                if(valid){
+                    if (editText(generateString(rows, columns))) {
+                        vscode.window.showInformationMessage('New markdown table was created...');
+                    }
+                }
+                  
+            });
+ 
+        });
 
     context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable1);
+}
+
+ 
+function ParseInput(value: string) {
+    var user_input;
+    var columns, rows; 
+    var valid : boolean;
+    
+    var regexp = new RegExp('[0-9]+(,[0-9]+)');
+    if (regexp.test(value)) {
+        valid = true;
+        user_input = value.split(',');
+        rows = user_input[0];
+        columns = user_input[1];
+        return { valid, rows, columns };
+    }
+    else {
+        vscode.window.showInformationMessage('Wrong input format, please use \"Rows,Columns\"');
+        valid = false;
+        return {valid, rows, columns };
+    }
+   
 }
 
 // this method is called when your extension is deactivated
